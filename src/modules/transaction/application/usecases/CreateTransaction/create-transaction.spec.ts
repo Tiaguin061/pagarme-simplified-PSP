eaf04cc88eaeb68bf8c1ad3baaf162144a819fc3
Tiaguin061 <tiaguin180@gmail.com>
2023-04-09 13:54:59 -0300

    tests: Added tests for create transaction useCase

---
+++ src/modules/transaction/application/usecases/CreateTransaction/create-transaction.spec.ts
@@ -0,0 +1,41 @@
+import { beforeAll, describe, expect, it } from '@jest/globals';
+
+import { ITransactionRepository } from '@root/modules/transaction/domain/repositories/transaction-repository';
+import { InMemoryTransactionRepository } from '@root/modules/transaction/infra/repositories/in-memory/transactions-repository';
+import { CreateTransaction } from './create-transaction';
+
+let createTransaction: CreateTransaction;
+let transactionRepository: ITransactionRepository;
+
+describe('Create Transaction use-case', () => {
+  beforeAll(() => {
+    transactionRepository = new InMemoryTransactionRepository();
+    createTransaction = new CreateTransaction(transactionRepository);
+  })
+  it('Should be able to create', async () => {
+    const response = await createTransaction.execute({
+      card_expiration_date: new Date(),
+      card_holder_name: 'John Doe',
+      card_number: 123456789,
+      card_verification_code: 123,
+      payment_method: 'credit_card',
+      description: 'Fake description'
+    });
+
+    expect(await transactionRepository.exists(6789)).toBeTruthy();
+    expect(response.isRight()).toBeTruthy();
+  });
+
+  it('Should be able return error if card_number is invalid', async () => {
+    const response = await createTransaction.execute({
+      card_expiration_date: new Date(),
+      card_holder_name: 'John Doe',
+      card_number: Number('Invalid card number'),
+      card_verification_code: 123,
+      payment_method: 'credit_card',
+      description: 'Fake description'
+    });
+
+    expect(response.isLeft()).toBeTruthy();
+  });
+})
