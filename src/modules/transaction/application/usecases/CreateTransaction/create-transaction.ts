871f374f6e12e9816e8245b1423fb739a53d08cf
Tiaguin061 <tiaguin180@gmail.com>
2023-04-09 13:54:39 -0300

    feat: Added useCase for create an transaction

---
+++ src/modules/transaction/application/usecases/CreateTransaction/create-transaction.ts
@@ -0,0 +1,41 @@
+import { left, right } from '@root/core/logic/Either';
+import { PaymentMethod, Transaction } from '@root/modules/transaction/domain/entities/transaction/transaction';
+
+import { CardNumber } from '@root/modules/transaction/domain/entities/transaction/card-number';
+import { ITransactionRepository } from '@root/modules/transaction/domain/repositories/transaction-repository';
+
+type CreateTransactionRequest = {
+  description?: string;
+  payment_method: PaymentMethod;
+  card_number: number;
+  card_holder_name: string;
+  card_expiration_date: Date;
+  card_verification_code: number;
+}
+
+export class CreateTransaction {
+  constructor(
+    private transactionRepository: ITransactionRepository,
+  ) { }
+  async execute(data: CreateTransactionRequest) {
+    const { card_number } = data;
+
+    const cardNumberOrError = CardNumber.create(card_number);
+
+    if (cardNumberOrError.isLeft()) {
+      return left(cardNumberOrError.value);
+    }
+
+    const transactionOrError = Transaction.create({
+      ...data,
+      card_number: cardNumberOrError.value
+    });
+
+    const transaction = transactionOrError.value;
+
+    await this.transactionRepository.create(transaction);
+
+    return right(transaction);
+  }
+}
+
