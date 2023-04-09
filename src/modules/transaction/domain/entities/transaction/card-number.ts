9f00a8a4ec5244ba3b24f1a13d24b9a68eb02237
Tiaguin061 <tiaguin180@gmail.com>
2023-04-09 17:23:18 -0300

    chore: Used invalid card number error

--- src/modules/transaction/domain/entities/transaction/card-number.ts
+++ src/modules/transaction/domain/entities/transaction/card-number.ts
@@ -1,5 +1,7 @@
 import { Either, left, right } from '@root/core/logic/Either';
 
+import { InvalidCardNumberError } from './errors/InvalidCardNumberError';
+
 export class CardNumber {
   private readonly card_number: number;
 
@@ -11,8 +13,8 @@ export class CardNumber {
     return this.card_number;
   }
 
-  static checkIfCardNumberIsNAN(card_number: number) {
-    if(isNaN(card_number)) {
+  static checkIfCardNumberIsInvalid(card_number: number) {
+    if (isNaN(card_number)) {
       return true;
     }
 
@@ -23,11 +25,11 @@ export class CardNumber {
     const result = String(card_number).slice(-4);
 
     return Number(result);
-  } 
+  }
 
-  static create(card_number: number): Either<Error, CardNumber> {
-    if(this.checkIfCardNumberIsNAN(card_number)) {
-      return left(Error('a'));
+  static create(card_number: number): Either<InvalidCardNumberError, CardNumber> {
+    if (this.checkIfCardNumberIsInvalid(card_number)) {
+      return left(new InvalidCardNumberError(card_number));
     }
 
     card_number = this.getTheFourLastCardNumber(card_number);
