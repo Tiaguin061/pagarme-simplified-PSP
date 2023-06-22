f623abd7324ece3905e688768817af5baecd6d88
Tiaguin061 <tiaguin180@gmail.com>
2023-06-22 14:39:07 -0300

    test: List balance

---
+++ src/modules/payable/application/usecases/ListBalance/list-balance.spec.ts
@@ -0,0 +1,64 @@
+import { describe, expect, it } from '@jest/globals';
+
+import { CardNumber } from '@root/modules/transaction/domain/entities/transaction/card-number';
+import { IPayableRepository } from '@root/modules/payable/domain/repositories/payable-repository';
+import { InMemoryPayablesRepository } from '@root/modules/payable/infra/repositories/in-memory/payables-repository';
+import { ListBalance } from './list-balance';
+import { Payable } from '@root/modules/payable/domain/entities/payable';
+import { Transaction } from '@root/modules/transaction/domain/entities/transaction/transaction';
+
+let listBalance: ListBalance;
+let payableRepository: IPayableRepository;
+
+describe('List Balance usecase', () => {
+  payableRepository = new InMemoryPayablesRepository();
+
+  listBalance = new ListBalance(payableRepository);
+
+  it('it should be able list balance', async () => {
+    const transactionWithCreditCard = Transaction.create({
+      card_expiration_date: new Date(),
+      card_holder_name: 'John Doe',
+      card_number: CardNumber.create(123456789).value as CardNumber,
+      card_verification_code: 123,
+      payment_method: 'credit_card',
+      description: 'Fake description',
+      value: 10
+    }).value;
+
+    const transactionWithDebitCard = Transaction.create({
+      card_expiration_date: new Date(),
+      card_holder_name: 'John Doe',
+      card_number: CardNumber.create(123456788).value as CardNumber,
+      card_verification_code: 123,
+      payment_method: 'debit_card',
+      description: 'Fake description',
+      value: 10
+    }).value;
+
+    const payableWithWaitingFunds = new Payable({
+      payment_date: new Date(),
+      status: 'waiting_funds',
+      transaction_id: transactionWithCreditCard.id,
+      transaction: transactionWithCreditCard,
+    });
+
+    const payableWithPaid = new Payable({
+      payment_date: new Date(),
+      status: 'paid',
+      transaction_id: transactionWithDebitCard.id,
+      transaction: transactionWithDebitCard,
+    });
+
+    await payableRepository.create(payableWithWaitingFunds);
+    await payableRepository.create(payableWithPaid);
+
+    const response = await listBalance.execute();
+
+    expect(response.isRight()).toBe(true);
+    expect(response.value).toStrictEqual({
+      waiting_funds: [payableWithWaitingFunds],
+      available: [payableWithPaid],
+    });
+  });
+});
