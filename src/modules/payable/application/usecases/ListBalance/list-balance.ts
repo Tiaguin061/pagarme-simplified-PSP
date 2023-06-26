6ba228bcaf20514a45b24bd7415b596fef0811eb
Tiaguin061 <tiaguin180@gmail.com>
2023-06-25 21:01:42 -0300

    refactor: Return balance

--- src/modules/payable/application/usecases/ListBalance/list-balance.ts
+++ src/modules/payable/application/usecases/ListBalance/list-balance.ts
@@ -6,8 +6,14 @@ import { Payable } from '@root/modules/payable/domain/entities/payable';
 export type ListBalanceResponse = Either<
   null,
   {
-    available: Payable[],
-    waiting_funds: Payable[],
+    available: {
+      payables: Payable[];
+      balance: number;
+    };
+    waiting_funds: {
+      payables: Payable[];
+      balance: number;
+    };
   }
 >;
 
@@ -20,9 +26,23 @@ export class ListBalance {
     const available = await this.payableRepository.findManyByStatus('paid');
     const waiting_funds = await this.payableRepository.findManyByStatus('waiting_funds');
 
+    const availableBalance = available.reduce((acc, current) => {
+      return acc + current.transaction.value
+    }, 0);
+
+    const waitingFundsBalance = waiting_funds.reduce((acc, current) => {
+      return acc + current.transaction.value
+    }, 0);
+
     return right({
-      available,
-      waiting_funds
+      available: {
+        payables: available,
+        balance: availableBalance,
+      },
+      waiting_funds: {
+        payables: waiting_funds,
+        balance: waitingFundsBalance,
+      },
     });
   }
 }
